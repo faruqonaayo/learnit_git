0a683d3aeb713896c482f52537f7f9318009303f
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-10-19 13:03:39 -0500

    CREATE a HelloWorld.java file and IMPLEMENT a greeting message

---
+++ src/com/learnit/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.learnit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("Welcome to Learning Git.");
+	}
+
+}
