e1d5e0f1e813fd81d0800bef67b98f02fd45787a
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-10-19 15:31:27 -0500

    Spaces are added to the HelloWorld File

--- src/com/learnit/HelloWorld.java
+++ src/com/learnit/HelloWorld.java
@@ -3,6 +3,8 @@ package com.learnit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
+		
 		System.out.println("Hello, World!");
 		System.out.println("Welcome to Learning Git.");
 		System.out.println("This is my first Java program.");
